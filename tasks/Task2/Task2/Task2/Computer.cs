1f5ea766b990d1d82115ceee0a6df802a35bb81c
Hüttler Andreas <WRZAHT@wien.rbgat.net>
2018-05-14 15:42:30 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Task2/Computer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Computer
+    {
+
+        public Computer(int value_serNo, string value_manufacturer, decimal value_price)
+                {
+                    if (string.IsNullOrWhiteSpace(value_manufacturer)) throw new ArgumentException("Manufacturer must not be empty.");
+            p_Manufacturer = value_manufacturer;
+            SerNo = value_serNo; 
+            p_Price = value_price;
+
+                }
+
+        private decimal p_Price;
+        private string p_Manufacturer;
+        private decimal p_SerNo;
+        
+
+        public string Manufacturer {
+            get
+            {
+                return Manufacturer; 
+            }
+            set
+            {
+                Manufacturer = value;
+            }
+        }
+
+        public int SerNo
+        {
+            get; set;
+        }
+
+        public void updatePrice(decimal price)
+        {
+            p_Price = price;
+        }
+
+        public decimal getPrice()
+        {
+            return p_Price;
+        }
+
+        
+    }
+}
+
+
+
---
+++ tasks/Task2/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp1 = new Computer(12345, "Asus", 999);
+            Console.WriteLine("erster Computer: " + comp1.SerNo + " " + comp1.Manufacturer + " " + comp1.getPrice());
+
+
+        }
+    }
+}
+
+
