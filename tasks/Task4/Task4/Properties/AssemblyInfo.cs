ecfb9b451720a3c06cbed5bc096405f02a7bd15e
Andreas Huettler <andreas.huettler@gmail.com>
2018-05-24 14:33:10 +0200

    add project Task4

--- tasks/Task4/Task4/Properties/AssemblyInfo.cs
+++ tasks/Task4/Task4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle(title: "Task4")]
+[assembly: AssemblyTitle("Task2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Task4")]
+[assembly: AssemblyProduct("Task2")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
