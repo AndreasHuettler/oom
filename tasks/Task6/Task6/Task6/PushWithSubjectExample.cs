106d536d6bcfed8987e619617aa8c70375bbdb16
Andreas Huteller <andreas.huettler@gmail.com>
2018-06-10 23:03:14 +0200

    Task 6 editted

---
+++ tasks/Task6/Task6/Task6/PushWithSubjectExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace Task6
+{
+    class PushWithSubjectExample
+    {
+        public static void run()
+        {
+            var source = new Subject<Computer>();
+
+            source
+                .Sample(TimeSpan.FromSeconds(1.0))
+                .Subscribe(x => Console.WriteLine($"received: SerNo: " + x.SerNo + " Manufacturer: " + 
+                x.Manufacturer + " Price: " + x.Price))
+                ;
+
+           
+
+            var t = new Thread(() =>
+            {
+                var i = 0;
+                while (true)
+                {
+                    Computer comp = new Computer(i, "RandomManufacturer", 2999);
+                    Thread.Sleep(2500);
+                    source.OnNext(comp);
+                    Console.WriteLine($"sent {i}");
+                    i++;
+                }
+            });
+            t.Start();
+        }
+    }
+}
