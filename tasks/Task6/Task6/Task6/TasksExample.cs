653f8ac1161166bfb88be6ba975e8cedae3c9166
Andreas Huettler <ic17b061@technikum-wien.at>
2018-06-22 22:57:31 +0200

    done

--- tasks/Task6/Task6/Task6/TasksExample.cs
+++ tasks/Task6/Task6/Task6/TasksExample.cs
@@ -13,82 +13,31 @@ namespace Task6
         static Random random = new Random();
 
 
-        public static async void Run()
+        public static void Run()
         {
-            
-            
             int eingabe = 0;
-            var tasks = new List<Task>();
 
-            Console.WriteLine("Gib eine grosse Zahl ein: ");
+            Console.WriteLine("Enter a number (e.g.:  5  ): ");
             var stringeingabe = Console.ReadLine();
             if (int.TryParse(stringeingabe, out eingabe))
             {
-                
                 while (eingabe != 0)
                 {
+                    AsyncMethod();
                     
-                    
-                    /*
-                    if (await Calc())
-                    {
-                        var task = Task.Run(() =>
-                        {
-                            Console.WriteLine("[Waited Task] ... doing something");
-                        }).ContinueWith(TASK => Console.WriteLine("[ContinueWith Task " + tasknumber + "] ... continuing doing something."));
-                    }
-                    */
-
-                    /*
-                    var task = Task.Run(() =>
-                    {
-
-                        if(await Calc())
-                        {
-
-                        }
-                    }).ContinueWith(TASK => Console.WriteLine("[ContinueWith Task " + tasknumber + "] ... doing something.")); 
-
-
-                        tasks.Add(task);
-                    */
-
-
-
-
-                    /*
-                    foreach (var t in tasks) {
-                        t.ContinueWith(TASK => Console.WriteLine("[ContinueWith] ... doing something."));
-                    }
-                    */
                     Thread.Sleep(1000);
                     eingabe--;
-                    Console.WriteLine(eingabe);
+                    Console.WriteLine("Counter: " + eingabe);
                 }
-                var cts = new System.Threading.CancellationTokenSource();
             }
-            
-            
         }
-        /*
-        public static async Task<bool> divideByTwo(int eingabe, CancellationToken ct)
-            {
-
-             if((eingabe % 2) == 0)
-             {
-                ct.ThrowIfCancellationRequested();
-                if
-             }
-            }
-            */
-
+        
         public static Task<bool> Calc()
         {
             return Task.Run(() =>
              {
                  tasknumber++;
                  Console.WriteLine("[Task " + tasknumber + "] Calculating...");
-                 //Task.Delay(TimeSpan.FromSeconds(5.0 + random.Next(7))).Wait();
                  Console.WriteLine("[Task " + tasknumber + "] ... Calculation done");
                  return true;
              });
@@ -97,7 +46,13 @@ namespace Task6
 
         public static async void AsyncMethod()
         {
-            if (await Calc()) Console.WriteLine("true");
+            if (await Calc())
+            {
+                var task = Task.Run(() =>
+                {
+                    Console.WriteLine("[Waited Task] ... executed after Task" + tasknumber + " was finished");
+                }).ContinueWith(TASK => Console.WriteLine("[ContinueWith Task " + tasknumber + "] ... continuing doing something with the Waited Task."));
+            }
         }
     }
 }
